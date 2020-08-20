0d26c3d6f5c14bf2c26b6a241abc440311cc9454
codetyphon <oncwnuNcpkVqPNXxw_z6tJhS5ASE@git.weixin.qq.com>
2020-08-20 16:17:51 +0800

    fixbugs

--- src/App.js
+++ src/App.js
@@ -3,64 +3,77 @@ import {Launcher} from 'react-chat-window'
 import './App.css';
 
 function App() {
-  const [messageList,setMessageList] = useState([])
-  const [msg,setMsg]=useState("")
-  const [up,setUp]=useState(0)
-  const [isopen,setIsopen]=useState(false)
+  const [lastMessageTime,setLastMessageTime]=useState(0)//最后消息的时间
+  const [messageList,setMessageList]=useState([])
+  const [unReadCount,setUnReadCount]=useState(0)//未读消息
+  const [isOpen,setIsOpen]=useState(false)//聊天窗体是否打开
   const [isfirst,setIsfirst]=useState(true)
 
   const _onFilesSelected = files =>{
     console.log(files)
   }
 
-  const _onMessageWasSent = message=> {
+  const _on_botMessage_was_sent_to_user = message=> {
     console.log(message)
-    setMessageList([...messageList, message])
-    setMsg(message)
-    setUp(Date.now())
+    messageList.push(message)
+    setLastMessageTime(Date.now())
+    setTimeout(() => {
+      _bot_sendMessage_to_user(`${message.data[message.type]} too`)
+    }, 2000);
   }
 
-  const _sendMessage = text => {
+  const _bot_sendMessage_to_user = text => {
     if (text.length > 0) {
-      setMessageList([...messageList, {
+      messageList.push({
         author: 'them',
         type: 'text',
         data: { text }
-      }])
+      })
+      setLastMessageTime(Date.now())
     }
   }
 
-  const handleClick = () => {
+  const _onHandleClick = () => {
     console.log('lanch')
-    setIsopen(!isopen)
+    console.log(isOpen)
+    setIsOpen(!isOpen)
+  }
+
+  useEffect(()=>{
+    console.log(isOpen)
     if(isfirst){
+      _bot_sendMessage_to_user(`1`)
+      _bot_sendMessage_to_user(`2`)
       setIsfirst(false)
     }else{
       //不是第一次载入，则机器人不主动聊天。
     }
-  }
+    setUnReadCount(0)//只要打开了，未读消息就是0
+  },[isOpen])
 
+  //用这个进行触发，只有使用useState的set方法，才能让dom更新。
   useEffect(()=>{
-    if(up!=0){
-      setTimeout(() => {
-        _sendMessage(`${msg.data[msg.type]} too`)
-      }, 1000);
+    console.log("time up")
+    if(lastMessageTime){
+      setMessageList([...messageList])
     }
-  },[up])
+    setUnReadCount(unReadCount+1)
+  },[lastMessageTime])
 
   return (
     <div className="App">
       <header className="App-header">
         <Launcher
-        isOpen={isopen}
+        isOpen={isOpen}
         agentProfile={{
-          teamName: 'echo_bot',
+          teamName: 'chatbot',
           imageUrl: '/logo.png'
         }}
+        newMessagesCount={unReadCount}
         onFilesSelected={_onFilesSelected}
-        onMessageWasSent={_onMessageWasSent}
+        onMessageWasSent={_on_botMessage_was_sent_to_user}
         messageList={messageList}
-        handleClick={handleClick}
+        handleClick={_onHandleClick}
         showEmoji
       />
       </header>
