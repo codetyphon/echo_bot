04b330af2613172f0acd3c0e5cee9ebf4f425378
codetyphon <oncwnuNcpkVqPNXxw_z6tJhS5ASE@git.weixin.qq.com>
2020-08-20 16:49:54 +0800

    fixbugs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import {Launcher} from 'react-chat-window'
+import { useDynamicList } from 'ahooks';
 import './App.css';
 
 function App() {
+  const { list, remove, getKey, push } = useDynamicList([]);
   const [lastMessageTime,setLastMessageTime]=useState(0)//最后消息的时间
-  const [messageList,setMessageList]=useState([])
   const [unReadCount,setUnReadCount]=useState(0)//未读消息
   const [isOpen,setIsOpen]=useState(false)//聊天窗体是否打开
   const [isfirst,setIsfirst]=useState(true)
@@ -14,17 +15,17 @@ function App() {
   }
 
   const _on_botMessage_was_sent_to_user = message=> {
-    console.log(message)
-    messageList.push(message)
-    setLastMessageTime(Date.now())
+    console.log('user 2 bot')
+    push(message)
     setTimeout(() => {
       _bot_sendMessage_to_user(`${message.data[message.type]} too`)
     }, 2000);
   }
 
   const _bot_sendMessage_to_user = text => {
+    console.log('bot 2 user')
     if (text.length > 0) {
-      messageList.push({
+      push({
         author: 'them',
         type: 'text',
         data: { text }
@@ -53,10 +54,6 @@ function App() {
 
   //用这个进行触发，只有使用useState的set方法，才能让dom更新。
   useEffect(()=>{
-    console.log("time up")
-    if(lastMessageTime){
-      setMessageList([...messageList])
-    }
     setUnReadCount(unReadCount+1)
   },[lastMessageTime])
 
@@ -72,7 +69,7 @@ function App() {
         newMessagesCount={unReadCount}
         onFilesSelected={_onFilesSelected}
         onMessageWasSent={_on_botMessage_was_sent_to_user}
-        messageList={messageList}
+        messageList={list}
         handleClick={_onHandleClick}
         showEmoji
       />
